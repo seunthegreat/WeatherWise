df0146e0cdf3852caa43acdefbc291a3116372f7
Seun Abilawon <59948725+seunthegreat@users.noreply.github.com>
2023-03-21 07:35:54 -0700

    responsiveness

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -9,34 +9,38 @@ const {popularCities} = AppConstant
 const Sidebar: React.FC<ComponentProps['Sidebar']> = ({onSelectCity, currentSelection, screenSize}): JSX.Element => {
   
   return (
-    <div className={`${screenSize > 800 ? 'w-96' : 'w-72'} h-screen md:overflow-hidden shadow-2xl overflow-auto md:hover:overflow-auto p-3 z-10 bg-white`}>
-      <div className='mt-2 h-14 border-b-[1px] flex flex-row justify-between items-start'>
-        <p className={`${text.subHeading}`}>Weather Wise</p>
-
-        <button className=''>
-          <p className={`${text.body} bg-highlight p-1 px-2 rounded-[2px] text-white`}>Demo</p>
-        </button>
-      </div>
-
-      <div className='py-2'>
-         <Search onClickSearchResult={onSelectCity} />
-      </div>
-
-      <div className='my-2 flex flex-col h-[100%] w-full'>
-        <p className={`${text.body}`}>Popular Cities</p>
-
-        <div className='overflow-scroll mt-3 py-2'>
-          {popularCities.map((item, index) => (
-            <CityCard 
-              key={index} 
-              info={item}
-              currentCity={currentSelection} 
-              onSelectCity={onSelectCity}
-            />
-          ))} 
+    <div className='flex'>
+      {screenSize > 600 && (
+        <div className={`${screenSize > 768 ? 'w-96' : 'w-72'} h-screen md:overflow-hidden shadow-2xl overflow-auto md:hover:overflow-auto p-3 z-10 bg-white`}>
+        <div className='mt-2 h-14 border-b-[1px] flex flex-row justify-between items-start'>
+          <p className={`${text.subHeading}`}>Weather Wise</p>
+  
+          <button className=''>
+            <p className={`${text.body} bg-highlight p-1 px-2 rounded-[2px] text-white`}>Demo</p>
+          </button>
         </div>
+  
+        <div className='py-2'>
+           <Search onClickSearchResult={onSelectCity} />
+        </div>
+  
+        <div className='my-2 flex flex-col h-[100%] w-full'>
+          <p className={`${text.body}`}>Popular Cities</p>
+  
+          <div className='overflow-scroll mt-3 py-2'>
+            {popularCities.map((item, index) => (
+              <CityCard 
+                key={index} 
+                info={item}
+                currentCity={currentSelection} 
+                onSelectCity={onSelectCity}
+              />
+            ))} 
+          </div>
+        </div>
+  
       </div>
-
+      )}
     </div>
   )
 }
